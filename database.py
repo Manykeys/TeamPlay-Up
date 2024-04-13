f72dcfe9fa9c5dd14fc8417dd1265eff68729150
Firchalka <nikorlov26@gmail.com>
2024-04-13 22:59:33 +0500

    login and register

---
+++ database.py
@@ -0,0 +1,62 @@
+import sqlite3
+
+db1_connect = sqlite3.connect('users.db')
+db1_cursor = db1_connect.cursor()
+
+users_list = [
+    (1, 'Firchalka', 'Nikta2004'),
+    (2, 'Sanya', 'Artem13Chlen'),
+]
+
+
+def users_db(data_of_users):
+    db1_cursor.execute("""CREATE TABLE IF NOT EXISTS users(
+        user_id INTEGER PRIMARY KEY,
+        user_login TEXT,
+        user_password TEXT
+    )
+    """)
+    db1_cursor.executemany("INSERT INTO users VALUES(?,?,?);", users_list)
+    db1_connect.commit()
+
+
+def add_user(login, password):
+    with sqlite3.connect('users.db') as connection:
+        cursor = connection.cursor()
+        cursor.execute(
+            "INSERT INTO users (user_login, user_password) VALUES (?, ?);",
+            (login, password))
+        connection.commit()
+
+
+def search_users_db():
+    with sqlite3.connect('users.db') as connection:
+        cursor = connection.cursor()
+        cursor.execute(f"SELECT * FROM users")
+        all_results = cursor.fetchall()
+    return all_results
+
+
+def get_dictionary_of_users():
+    data = search_users_db()
+    result = {}
+    for user in data:
+        result[user[1]] = (user[2], user[0])
+    return result
+
+
+#
+# users_db(users_list)
+
+def view_users():
+    db1_cursor.execute("SELECT * FROM users")
+    rows = db1_cursor.fetchall()
+    if rows:
+        print("User ID | User Login | User Password")
+        print("-------------------------------------")
+        for row in rows:
+            print(f"{row[0]} | {row[1]} | {row[2]}")
+    else:
+        print("Таблица пользователей пуста.")
+
+# view_users()
---
+++ main.py
@@ -0,0 +1,32 @@
+from flask import Flask, request, render_template, redirect, url_for
+import database
+
+app = Flask(__name__)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        users = database.get_dictionary_of_users()
+        if username in users and users[username][0] == password:
+            print(1)
+            return render_template('login.html', error=True)
+        else:
+            return render_template('login.html', error=True)
+    else:
+        return render_template('login.html', error=False)
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        database.add_user(username, password)
+    return render_template('login.html', error=True)
+
+
+if __name__ == '__main__':
+    app.run(host="127.0.0.1", port=5000)
