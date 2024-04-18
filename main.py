0eb865b5aae872a07fe0eadc4878c779c2525bc9
Firchalka <nikorlov26@gmail.com>
2024-04-18 21:47:07 +0500

    chipi chipi

--- main.py
+++ main.py
@@ -4,17 +4,25 @@ import database
 app = Flask(__name__)
 
 
+@app.route('/', methods=['GET', 'POST'])
+def main():
+    return render_template('main_page.html', error=False)
+
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
         users = database.get_dictionary_of_users()
+        print(users)
         if username in users and users[username][0] == password:
-            return render_template('main_page.html')
+            print(2)
+            return render_template('menu.html')
         else:
             error_message = 'Incorrect username or password. Please try again.'
-            return render_template('login.html', error=True, error_message=error_message)
+            return render_template('login.html', error=True,
+                                   error_message=error_message)
     else:
         return render_template('login.html', error=False)
 
@@ -27,7 +35,8 @@ def register():
         dict = database.get_dictionary_of_users()
         if username in dict.keys():
             error_message = 'Nickname already exists. Please choose a different one.'
-            return render_template('registration.html', error=True, error_message=error_message)
+            return render_template('registration.html', error=True,
+                                   error_message=error_message)
         else:
             database.add_user(username, password)
             return render_template('main_page.html')
