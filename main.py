50145c16323f14f99e12e7cf398970cb84bcae5f
Ga1kinSasha <Galkin.Alexander@at.urfu.ru>
2024-05-07 22:02:43 +0500

    Update main.py
    сделать чтобы левый чел без аутентификации не мог зайти

--- main.py
+++ main.py
@@ -2,12 +2,11 @@ from flask import Flask, request, render_template, redirect, url_for
 import database
 
 app = Flask(__name__)
-
+is_authorized = False
 
 @app.route('/', methods=['GET', 'POST'])
 def main():
     return render_template('main_page.html', error=False)
-
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -17,15 +16,14 @@ def login():
         users = database.get_dictionary_of_users()
         print(users)
         if username in users and users[username][0] == password:
-            print(2)
-            return render_template('menu.html')
+            global is_authorized
+            is_authorized = True
+            return redirect(url_for('choose'))  # Redirect to the choose page after successful login
         else:
             error_message = 'Incorrect username or password. Please try again.'
-            return render_template('login.html', error=True,
-                                   error_message=error_message)
+            return render_template('login.html', error=True, error_message=error_message)
     else:
         return render_template('login.html', error=False)
-
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
@@ -35,21 +33,27 @@ def register():
         dict = database.get_dictionary_of_users()
         if username in dict.keys():
             error_message = 'Nickname already exists. Please choose a different one.'
-            return render_template('registration.html', error=True,
-                                   error_message=error_message)
+            return render_template('registration.html', error=True, error_message=error_message)
         else:
             database.add_user(username, password)
-            return render_template('main_page.html')
+            global is_authorized
+            is_authorized = True
+            return redirect(url_for('main'))  # Redirect to the main page after successful registration
     return render_template('registration.html', error=False)
 
 @app.route('/choose', methods=['GET', 'POST'])
 def choose():
-    return render_template('choose_desteny.html')
+    if is_authorized:
+        return render_template('choose_desteny.html')
+    else:
+        return redirect(url_for('login'))  # Redirect to the login page if not authorized
 
 @app.route('/dota2', methods=['GET', 'POST'])
 def dota2():
-    return render_template('dota2.html')
-
+    if is_authorized:
+        return render_template('dota2.html')
+    else:
+        return redirect(url_for('login'))  # Redirect to the login page if not authorized
 
 if __name__ == '__main__':
     app.run(host="127.0.0.1", port=5000)
