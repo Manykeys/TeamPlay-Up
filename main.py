5c33f77a72a5722beca0844ac56f8ef76fe2a5f6
bober19 <115493537+Manykeys@users.noreply.github.com>
2024-05-12 20:14:28 +0500

    Update main.py

--- main.py
+++ main.py
@@ -104,7 +104,7 @@ def on_join(data):
 def on_leave(data):
     chat_id = data['chat_id']
     leave_room(chat_id)
-    
+
 
 if __name__ == '__main__':
     socketio.run(app, host="127.0.0.1", port=5000, allow_unsafe_werkzeug=True)
